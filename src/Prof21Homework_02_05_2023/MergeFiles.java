6672af1cf7e46c35f55a820e246f1470f0574ef3
Anton Vouk <avo94eu@gmail.com>
2023-05-14 12:48:34 -0600

    Refactor code

--- src/Prof21Homework_02_05_2023/MergeFiles.java
+++ src/Prof21Homework_02_05_2023/MergeFiles.java
@@ -1,42 +1,38 @@
 package Prof21Homework_02_05_2023;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class MergeFiles {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String inputData = "A_B_C_D_E";
 
-        String path = Paths.get("C:", "Users", "Anton", "Downloads", "merge").toString();
+        Path path = Paths.get("C:", "Users", "Anton", "Downloads", "merge");
         String[] dataSplit = inputData.split("_");
-        Arrays.stream(dataSplit).
-                forEach(data -> createFile(Paths.get(path, data + ".txt").toString(), data));
+        Arrays.stream(dataSplit).forEach(data -> createFile(path, data + ".txt", data));
 
-        File[] files = new File(path).listFiles();
         StringBuilder result = new StringBuilder();
-        Arrays.stream(files).map(inputPath -> getDataFromFile(inputPath.toString())).forEach(result::append);
-        createFile(Paths.get(path, inputData + ".txt").toString(), result.toString());
+        Files.list(path).map(inputPath -> getDataFromFile(inputPath)).forEach(result::append);
+        createFile(path, inputData + ".txt", result.toString());
     }
 
-    private static void createFile(String path, String content) {
-        try (FileWriter fileWriter = new FileWriter(path)) {
-            fileWriter.write(content);
+    private static void createFile(Path path, String file, String content) {
+        try {
+            Files.createDirectories(path);
+            Path of = Path.of(path.toString(), file);
+            Files.writeString(of, content);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static String getDataFromFile(String path) {
-        try (FileReader reader = new FileReader(path)) {
-            StringBuilder sb = new StringBuilder();
-            while (reader.ready()) {
-                sb.append((char) reader.read());
-            }
-            return sb.toString();
+    private static String getDataFromFile(Path path) {
+        try {
+            return String.join("", Files.readAllLines(path));
         } catch (Exception e) {
             e.printStackTrace();
         }
